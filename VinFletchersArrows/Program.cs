b8d8f8f96f22c0a0e8fd259d986e35c5fad6ff1e
Boxfriend <cawills394@gmail.com>
2022-02-12 02:06:18 -0500

    The Properties of Arrows and Arrow Factories challenges

--- VinFletchersArrows/Program.cs
+++ VinFletchersArrows/Program.cs
@@ -1,23 +1,49 @@
 ﻿Console.Title = "Vin Fletcher's Arrow Shop";
 
-//pseudo-fields
+//pseudo-fields used for pretty menu options
+var options = new string[] { "Buy Premade Arrow", "Build Custom Arrow" };
 var heads = new string[] { "Steel", "Wood", "Obsidian" };
 var fletchings = new string[] { "Plastic", "Turkey Feathers", "Goose Feathers" };
+var premades = new string[] { "Elite Arrow", "Beginner Arrow", "Marksman Arrow" };
 
 Console.WriteLine("Welcome to Vin Fletcher's Arrow Shop, let's build your dream arrow!");
-Console.WriteLine("The Arrow Heads we currently have in stock are: ");
-DisplayMenu(heads);
-var head = GetInput("Which Arrow Head would you like? ",0, heads.Length);
-Console.Clear();
-Console.WriteLine("These are the Fletchings we have in stock: ");
-DisplayMenu(fletchings);
-var fletching = GetInput("Which Fletching would you like? ", 0, fletchings.Length);
-Console.Clear();
-var shaft = GetInput("How long would you like the arrow shaft to be? (60 - 100)", 60, 100);
-var arrow = new Arrow(shaft, IntToArrowHead(head), IntToFletching(fletching));
+DisplayMenu(options);
+var optionInput = GetInput("Which would you like? ",0, options.Length);
 
-//This line has also been modified for Vin's Trouble challenge
-Console.WriteLine($"You have chosen a {arrow.GetArrowHead()} tipped arrow with {arrow.GetFletching()} fletching that is {arrow.GetLength()}cm long. That will be {arrow.GetCost():#.##} gold.");
+var arrow = optionInput == (int)Options.Premade ? SelectPremadeArrow() : BuildCustomArrow();
+
+//This line has also been modified for Vin's Trouble challenge and again for The Properties of Arrows challenge
+Console.WriteLine($"You have chosen a {arrow.ArrowHead} tipped arrow with {arrow.Fletching} fletching that is {arrow.Length}cm long. That will be {arrow.Cost:#.##} gold.");
+
+//These next two methods are not very pretty, but until we're making multiple classes this is how they will stay
+Arrow BuildCustomArrow()
+{
+    Console.WriteLine("The Arrow Heads we currently have in stock are: ");
+    DisplayMenu(heads);
+    var headInput = GetInput("Which Arrow Head would you like? ", 0, heads.Length);
+    Console.Clear();
+    Console.WriteLine("These are the Fletchings we have in stock: ");
+    DisplayMenu(fletchings);
+    var fletchingInput = GetInput("Which Fletching would you like? ", 0, fletchings.Length);
+    Console.Clear();
+    var shaft = GetInput("How long would you like the arrow shaft to be? (60 - 100)", 60, 100);
+    return new Arrow(shaft, IntToArrowHead(headInput), IntToFletching(fletchingInput));
+}
+
+Arrow SelectPremadeArrow()
+{
+    Console.WriteLine("The premade arrows currently in stock are: ");
+    DisplayMenu(premades);
+    var input = GetInput("Which Arrow would you like? ",0,premades.Length);
+
+    return input switch
+    {
+        1 => Arrow.CreateEliteArrow(),
+        2 => Arrow.CreateBeginnerArrow(),
+        3 => Arrow.CreateMarksmanArrow(),
+        _ => Arrow.CreateEliteArrow()
+    };
+}
 
 int GetInput (string text, int min, int max)
 {
@@ -32,6 +58,7 @@ void DisplayMenu(string[] menu)
     {
         Console.WriteLine($"{i+1} - {menu[i]}");
     }
+    Console.WriteLine($"Select an option using a number from 1 - {menu.Length}");
 }
 
 ArrowHead IntToArrowHead(int head)
@@ -41,7 +68,7 @@ ArrowHead IntToArrowHead(int head)
         1 => ArrowHead.Steel,
         2 => ArrowHead.Wood,
         3 => ArrowHead.Obsidian,
-        _ => ArrowHead.Steel
+        _ => ArrowHead.Steel //Not necessary because input is limited, but i don't like disabling warnings so this is only here to make the compiler happy
     };
 }
 
@@ -52,16 +79,22 @@ Fletching IntToFletching(int fletching)
         1 => Fletching.Plastic,
         2 => Fletching.TurkeyFeathers,
         3 => Fletching.GooseFeathers,
-        _ => Fletching.Plastic
+        _ => Fletching.Plastic //Ditto previous comment
     };
 }
 
-//Modified for Vin's Trouble challenge
+//Modified for Vin's Trouble challenge and again for The Properties of Arrows challenge and finally for the Arrow Factories challenge
 class Arrow
 {
     private int _shaftLength;
     private ArrowHead _arrowHead;
     private Fletching _fletching;
+
+    public ArrowHead ArrowHead => _arrowHead;
+    public Fletching Fletching => _fletching;
+    public int Length => _shaftLength;
+
+    public float Cost => (int)_arrowHead + (int)_fletching + (_shaftLength * 0.05f);
 
     public Arrow(int shaftLength, ArrowHead arrowHead, Fletching fletching)
     {
@@ -70,10 +103,9 @@ class Arrow
         _fletching = fletching;
     }
 
-    public float GetCost () => (int)_arrowHead + (int)_fletching + (_shaftLength * 0.05f);
-    public int GetLength () => _shaftLength;
-    public ArrowHead GetArrowHead() => _arrowHead;
-    public Fletching GetFletching() => _fletching;
+    public static Arrow CreateEliteArrow () => new (95, ArrowHead.Steel, Fletching.Plastic);
+    public static Arrow CreateBeginnerArrow () => new (75, ArrowHead.Wood, Fletching.GooseFeathers);
+    public static Arrow CreateMarksmanArrow () => new (65, ArrowHead.Obsidian, Fletching.TurkeyFeathers);
 }
 enum ArrowHead
 {
@@ -86,4 +118,10 @@ enum Fletching
     Plastic = 10,
     TurkeyFeathers = 5,
     GooseFeathers = 3
+}
+
+enum Options
+{
+    Premade = 1,
+    Custom = 2
 }

4a52dbab1929343c2d76068265a0d4d45421c7af
Boxfriend <cawills394@gmail.com>
2022-02-11 19:42:09 -0500

    reduced .ToLower() calls in SimulasTest, completed SimulasSoup

---
+++ SimulasSoup/Program.cs
@@ -0,0 +1,40 @@
+﻿var soups = new string[] { "Soup", "Stew", "Gumbo" };
+var ingredients = new string[] { "Mushrooms", "Chicken", "Carrots", "Potatoes" };
+var seasonings = new string[] { "Spicy", "Salty", "Sweet" };
+
+Console.Title = "Simula's Soup Menu";
+Console.WriteLine("Today's Menu:");
+DisplayMenu(soups);
+var soup = GetValidInput(0,soups.Length);
+Console.Clear();
+Console.WriteLine("Today's Ingredients:");
+DisplayMenu(ingredients);
+var ingredient = GetValidInput(0, ingredients.Length);
+Console.Clear();
+Console.WriteLine("Today's Seasonings:");
+DisplayMenu(seasonings);
+var seasoning = GetValidInput(0, seasonings.Length);
+var recipe = GetRecipe(seasoning, ingredient,soup);
+Console.WriteLine($"For today's meal you have chosen {recipe.seasoning} {recipe.ingredient} {recipe.recipe}");
+
+(Seasoning seasoning, Ingredient ingredient, Recipe recipe) GetRecipe(int seasoning, int ingredient, int soup)
+{
+    var recipe = (Recipe)soup;
+    var ingredients = (Ingredient)ingredient;
+    var seasonings = (Seasoning)seasoning;
+    return (seasonings, ingredients, recipe);
+}
+int GetValidInput(int min, int max)
+{
+    Console.Write("Which menu item would you like? ");
+    var input = Convert.ToInt32(Console.ReadLine());
+    return input > min && input <= max ? input-1 : GetValidInput(min, max);
+}
+void DisplayMenu (string[] menu)
+{
+    for(var i = 0; i < menu.Length; i++)
+        Console.WriteLine($"{i+1} - {menu[i]}");
+}
+enum Recipe { Soup, Stew, Gumbo }
+enum Ingredient { Mushrooms, Chicken, Carrots, Potatoes }
+enum Seasoning { Spicy, Salty, Sweet }
--- SimulasTest/Program.cs
+++ SimulasTest/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var chestState = LockState.Locked;
+Console.Title = "Simula's Test";
+while(true)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.Write($"The chest is {GetStateAsString(chestState)}. What do you want to do? ");
+    var input = Console.ReadLine();
+    if(String.IsNullOrWhiteSpace(input) || !IsValidCommand(input.ToLower()))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid Input. Try again.");
+        continue;
+    }
+    input = input.ToLower();
+    if (!IsValidTransition(chestState,input))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("You cannot do that right now.");
+        continue;
+    }
+
+    chestState = GetNewState(input);
+}
+
+bool IsValidCommand(string command) => command == "unlock" || command == "lock" || command == "open" || command == "close"; 
+
+bool IsValidTransition(LockState state, string command)
+{
+    return state switch
+    {
+        LockState.Locked => command == "unlock",
+        LockState.Unlocked => command == "open" || command == "lock",
+        LockState.Open => command == "close"
+    };
+}
+
+LockState GetNewState(string command)
+{
+    return command switch
+    {
+        "open" => LockState.Open,
+        "close" => LockState.Unlocked,
+        "unlock" => LockState.Unlocked,
+        "lock" => LockState.Locked
+    };
+}
+
+string GetStateAsString(LockState state)
+{
+    return state switch
+    {
+        LockState.Locked => "locked",
+        LockState.Unlocked => "unlocked",
+        LockState.Open => "open"
+    };
+}
+
+enum LockState { Locked, Unlocked, Open }

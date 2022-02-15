deebfd662fff08fe31691619107d142a4e3987b5
Boxfriend <cawills394@gmail.com>
2022-02-15 01:48:28 -0500

    completed The Old Robot

---
+++ TheOldRobot/Commands.cs
@@ -0,0 +1,55 @@
+﻿namespace TheOldRobot;
+
+internal class OnCommand : RobotCommand
+{
+    public override void Run (Robot bot) => bot.IsPowered = true;
+}
+
+internal class OffCommand : RobotCommand
+{
+    public override void Run (Robot bot) => bot.IsPowered = false;
+}
+
+internal class NorthCommand : RobotCommand
+{
+    public override void Run (Robot bot)
+    {
+        if(!bot.IsPowered) 
+            return;
+
+        bot.Y += 1;
+    }
+}
+
+internal class SouthCommand : RobotCommand
+{
+    public override void Run (Robot bot)
+    {
+        if (!bot.IsPowered)
+            return;
+
+        bot.Y -= 1;
+    }
+}
+
+internal class WestCommand : RobotCommand
+{
+    public override void Run (Robot bot)
+    {
+        if (!bot.IsPowered)
+            return;
+
+        bot.X -= 1;
+    }
+}
+
+internal class EastCommand : RobotCommand
+{
+    public override void Run (Robot bot)
+    {
+        if (!bot.IsPowered)
+            return;
+
+        bot.X += 1;
+    }
+}
---
+++ TheOldRobot/Program.cs
@@ -0,0 +1,38 @@
+﻿using TheOldRobot;
+
+Console.Title = "The Old Robot";
+var robot = new Robot();
+Console.WriteLine("You come across an old robot. You may give it three commands.");
+
+for (var i = 0; i < robot.Commands.Length; i++)
+{
+    var input = GetInput(i + 1);
+    robot.Commands[i] = RobotCommand.GetCommandFromInput(input);
+}
+
+robot.Run();
+
+
+string GetInput (int commandNumber)
+{
+    var input = "";
+    while(!ValidateInput(input!))
+    {
+        Console.WriteLine("Valid Commands: On Off North South East West");
+        Console.Write($"Input command number {commandNumber}: ");
+        input = Console.ReadLine();
+    }
+    return input;
+}
+
+bool ValidateInput(string input)
+{
+    if (string.IsNullOrEmpty(input))
+        return false;
+
+    input = input.ToLower().Trim();
+
+    return input == "on" || input == "off" || 
+        input == "north" || input == "south" || 
+        input == "east" || input == "west";
+}

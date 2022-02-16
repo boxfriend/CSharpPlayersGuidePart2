6d36d1bb7c52ad7e47f5ef4aa81ce50f960068b4
Boxfriend <cawills394@gmail.com>
2022-02-16 12:38:33 -0500

    War Preparations challenge. records are neat.

---
+++ WarPreparations/Program.cs
@@ -0,0 +1,9 @@
+﻿using WarPreparations;
+
+var originalSword = new Sword(SwordMaterial.Iron, Gemstone.None);
+var diamondSword = originalSword with { Stone = Gemstone.Diamond };
+var steelSword = originalSword with { Material = SwordMaterial.Steel };
+
+Console.WriteLine(originalSword);
+Console.WriteLine(diamondSword);
+Console.WriteLine(steelSword);
---
+++ WarPreparations/Sword.cs
@@ -0,0 +1,22 @@
+﻿namespace WarPreparations;
+
+internal record class Sword(SwordMaterial Material, Gemstone Stone);
+
+enum SwordMaterial
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+enum Gemstone
+{
+    None,
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone
+}

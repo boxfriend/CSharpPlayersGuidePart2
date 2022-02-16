fa193481845baa8099de3a567bd0cd94f55fceb3
Boxfriend <cawills394@gmail.com>
2022-02-16 09:18:42 -0500

    Room Coordinate challenge completed

---
+++ RoomCoordinates/Coordinate.cs
@@ -0,0 +1,27 @@
+﻿namespace RoomCoordinates;
+
+internal readonly struct Coordinate
+{
+    public int Row { get; init; }
+    public int Column { get; init; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public bool IsAdjacent(Coordinate other)
+    {
+
+        if(Row == other.Row)
+            return other.Column == Column + 1 || other.Column == Column - 1;
+
+        if(Column == other.Column)
+            return other.Row == Row + 1 || other.Row == Row - 1;
+
+        return false;
+    }
+
+    public override string ToString () => $"({Row}, {Column})";
+}
---
+++ RoomCoordinates/Program.cs
@@ -0,0 +1,17 @@
+﻿using RoomCoordinates;
+
+var rng = new Random();
+var coords = new Coordinate[10];
+for(var i = 0; i < 10; i++)
+    coords[i] = new Coordinate(rng.Next(0,10), rng.Next(0,10));
+
+foreach(var coord in coords)
+    Console.WriteLine(coord);
+
+for(var j = 0; j < 10; j++)
+{
+    var coord1 = coords[rng.Next(0,coords.Length)];
+    var coord2 = coords[rng.Next(0, coords.Length)];
+    Console.WriteLine($"Coordinate {coord1} adjacent to {coord2}? {coord1.IsAdjacent(coord2)}");
+}
+

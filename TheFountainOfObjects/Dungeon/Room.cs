67959d5ce6d8426d4a391d9056d6fff438851509
Boxfriend <cawills394@gmail.com>
2022-02-19 08:25:09 -0500

    rooms now have a RoomActions property

--- TheFountainOfObjects/Dungeon/Room.cs
+++ TheFountainOfObjects/Dungeon/Room.cs
@@ -4,64 +4,49 @@ internal class Room
 {
     public RoomType Type { get; private set; } = RoomType.Empty;
 
+    private RoomActions _roomActions = new EmptyRoom();
+
     public bool IsVisited { get; private set; }
 
     public bool TryChangeRoom(RoomType newType)
     {
+        //Prevents overwriting a non-empty room with a non-empty room
         if (Type != RoomType.Empty && newType != RoomType.Empty)
             return false;
 
+        //Obviously we don't want the fountain to move
         if(Type == RoomType.Fountain)
             return false;
 
         Type = newType;
+        _roomActions = TypeToActions(newType);
         return true;
     }
 
     public void VisitRoom()
     {
         IsVisited = true;
-
-        switch (Type)
-        {
-            case RoomType.Fountain:
-                Console.WriteLine("You hear rushing water. The fountain must be here!");
-                break;
-            default:
-                break;
-        }
+        _roomActions.RoomAction();
     }
 
-    public void AdjacentAction ()
-    {
-        switch (Type)
-        {
-            case RoomType.Fountain:
-                Console.WriteLine("You hear a faint dripping from a nearby room");
-                break;
-            default:
-                break;
-        }
-    }
+    public void AdjacentAction () => _roomActions.AdjacentAction();
 
-    public void DiagnonalAction()
-    {
-        switch (Type)
-        {
-            default:
-                break;
-        }
-    }
+    public void DiagonalAction () => _roomActions.DiagonalAction();
 
     public override string ToString ()
     {
         if (!IsVisited)
             return "?";
 
-        return Type switch
-        {
-            RoomType.Fountain => "F",
-            _ => " "
+        return _roomActions.Display();
+    }
+
+    private RoomActions TypeToActions(RoomType type)
+    {
+        return type switch
+        { 
+            RoomType.Fountain => new FountainRoom(),
+            _ => new EmptyRoom()
         };
     }
 }
---
+++ TheFountainOfObjects/Dungeon/RoomActions.cs
@@ -0,0 +1,21 @@
+﻿namespace TheFountainOfObjects;
+
+//Debated whether to make this a class or have some interfaces, figured class would be easiest so I don't have a whole bunch of unused members in an interface
+//and also so i don't have to add even more switches/conditions to determine whether a given room has a specific interface or not
+internal abstract class RoomActions
+{
+    public virtual void RoomAction () { return; }
+    public virtual void AdjacentAction () { return; }
+    public virtual void DiagonalAction () { return; }
+    public virtual string Display () => " ";
+}
+
+internal class EmptyRoom : RoomActions { }
+
+internal class FountainRoom : RoomActions
+{
+    public override void RoomAction () => Console.WriteLine("You hear rushing water nearby. The fountain must be here!");
+    public override void AdjacentAction () => Console.WriteLine("You hear a faint dripping from a nearby room");
+    public override string Display () => "F";
+}
+

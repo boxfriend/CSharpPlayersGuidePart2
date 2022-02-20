b10e2f3dca2826aaa8aaa84a62200c07960f2a2e
Boxfriend <cawills394@gmail.com>
2022-02-20 04:22:21 -0500

    Lists of commands. The Old Robot now uses a List<> instead of Array

--- TheOldRobot/Program.cs
+++ TheOldRobot/Program.cs
@@ -2,12 +2,17 @@
 
 Console.Title = "The Old Robot";
 var robot = new Robot();
-Console.WriteLine("You come across an old robot. You may give it three commands.");
+Console.WriteLine("You come across an old robot. You may give it commands to operate.");
 
-for (var i = 0; i < robot.Commands.Length; i++)
+var i = 1;
+while(true)
 {
-    var input = GetInput(i + 1);
-    robot.Commands[i] = Robot.GetCommandFromInput(input);
+    var input = GetInput(i++);
+
+    if (input == "stop")
+        break;
+
+    robot.Commands.Add(Robot.GetCommandFromInput(input));
 }
 
 robot.Run();
@@ -19,6 +24,7 @@ string GetInput (int commandNumber)
     while(!ValidateInput(input!))
     {
         Console.WriteLine("Valid Commands: On Off North South East West");
+        Console.WriteLine("Enter Stop if you wish to stop entering commands.");
         Console.Write($"Input command number {commandNumber}: ");
         input = Console.ReadLine();
     }
@@ -34,5 +40,5 @@ bool ValidateInput(string input)
 
     return input == "on" || input == "off" || 
         input == "north" || input == "south" || 
-        input == "east" || input == "west";
+        input == "east" || input == "west" || input == "stop";
 }
--- TheOldRobot/Robot.cs
+++ TheOldRobot/Robot.cs
@@ -5,7 +5,7 @@ internal class Robot
     public int X { get; set; }
     public int Y { get; set; }
     public bool IsPowered { get; set; }
-    public IRobotCommand?[] Commands { get; } = new IRobotCommand?[3];
+    public List<IRobotCommand> Commands { get; } = new ();
     public void Run()
     {
         foreach(IRobotCommand? command in Commands)
